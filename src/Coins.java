666e3cb1b77b065a5953eb72f3bad2b7af8d8e4f
Alex Harry <alexander.harry1@ecat.montana.edu>
2018-04-14 11:29:41 -0600

    Changed "=" to "==" on line 19

--- src/Coins.java
+++ src/Coins.java
@@ -16,7 +16,7 @@ public class Coins {
         changeArray[1] = 1; //instantiates the 1 spot
         for (int i = 2; i < changeArray.length; i++) {  //loops through the change array starting at location 2
             for (int j = coinOptions.length - 1; j >= 0; j--) { //loops through the denominations, starting with the highest
-                if (changeArray[i] % coinOptions[j] = 0) { //checks what denomination is divisiable by the location i in the array
+                if (changeArray[i] % coinOptions[j] == 0) { //checks what denomination is divisiable by the location i in the array
                     changeArray[i] = coinOptions[j];    //sets location of change array to the denomination chosen
                     break;  //breaks out of inner loop once location in array is set
                 }
