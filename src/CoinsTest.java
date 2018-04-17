601b75b95c3333f4d937deb488c9c23143a4530f
Alex Harry <alexander.harry1@ecat.montana.edu>
2018-04-17 15:14:22 -0600

    Recreated the test file, Previous file was throwing errors.

--- src/CoinsTest.java
+++ src/CoinsTest.java
@@ -1,19 +1,12 @@
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(Arquillian.class)
-public class CoinsTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(Coins.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+class CoinsTest {
+    @Test
+    void testInClassExampleMakeChangeOf16(){
+
     }
+
 
 }
