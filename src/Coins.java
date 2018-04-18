0e889596915d0c9eaff089170e6adb58deb3e4c9
Cory Johns <cory@triplevisiongroup.com>
2018-04-18 17:42:29 -0600

    Added tests:
    - added test for null input
    - added test for impossible input
    - added test for very large input
    - added fail for unimplemented test

--- src/Coins.java
+++ src/Coins.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,9 +10,23 @@ public class Coins {
     }
 
     public  int[]  makeChange(int[] coinOptions, int changeAmount) {
-        Arrays.sort(coinOptions);   //sorts the coin options from least to greatest
-        this.setupChangeArray(coinOptions);
-        return null;
+    	// we need some coins to be able to make change...
+		if (coinOptions == null || coinOptions.length == 0) {
+			throw new IllegalArgumentException("Must have at least one coin type");
+		}
+		else {
+	        Arrays.sort(coinOptions);   //sorts the coin options from least to greatest
+	        this.setupChangeArray(coinOptions);
+	        
+	        ArrayList<Integer> changeList = getChange(changeAmount);
+	        int[] change_int_list = new int[changeList.size()];
+	        // translate the array list to an int array
+	        for (int i=0; i<changeList.size(); i++) {
+	        	change_int_list[i] = changeList.get(i);
+	        }
+	        // return the change
+	        return change_int_list;
+		}
     }
 
     private void setupChangeArray(int[] coinOptions) {
@@ -21,7 +34,7 @@ public class Coins {
         changeArray[1] = 1; //instantiates the 1 spot
         for (int i = 2; i < changeArray.length; i++) {  //loops through the change array starting at location 2
             for (int j = coinOptions.length - 1; j >= 0; j--) { //loops through the denominations, starting with the highest
-                if (changeArray[i] % coinOptions[j] == 0) { //checks what denomination is divisiable by the location i in the array
+                if (changeArray[i] % coinOptions[j] == 0) { //checks what denomination is divisible by the location i in the array
                     changeArray[i] = coinOptions[j];    //sets location of change array to the denomination chosen
                     break;  //breaks out of inner loop once location in array is set
                 }
@@ -33,7 +46,6 @@ public class Coins {
         while (makeChangeof >= 0) {//loops until correct change has been made
             changeList.add(changeArray[makeChangeof]);  //adds the coin used at that array location to an arraylist
             makeChangeof -= changeArray[makeChangeof];  //removes coin used from makeChangeof to update the array location on the next loop
-
         }
         return changeList;
     }
--- src/TestCoins.java
+++ src/TestCoins.java
@@ -1,7 +1,7 @@
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+import org.junit.jupiter.api.BeforeAll;
 
 class TestCoins {
 	private static Coins testCoins;
@@ -13,7 +13,7 @@ class TestCoins {
 	
     @Test
     void testInClassExampleMakeChangeOf16(){
-
+    	fail("Not yet implemented");
     }
     
     @Test 
@@ -24,18 +24,68 @@ class TestCoins {
 		// test correct error message is generated
 		assertEquals("Must have at least one coin type", e.getMessage());
 	}
+    
+    @Test
+    void testNullInput() {
+    	// test  exception is thrown when given null
+		Exception e = assertThrows(IllegalArgumentException.class, () -> {  testCoins.makeChange(null, 221); } );
+		// test correct error message is generated
+		assertEquals("Must have at least one coin type", e.getMessage());
+    }
+    
+    @Test
+    void testReallyLargeInput() {
+    	Random rand = new Random();
+    	int[] coins = new int[rand.nextInt(rand.nextInt(100) * rand.nextInt(100)) + 2];
+    	int change = rand.nextInt(90000);
+    	// set up coins
+    	for (int i=0; i<coins.length; i++) {
+    		if (i>0) {
+    			// increment denomination by as much as 15 but no less then 1
+    			coins[i] = coins[i -1] + rand.nextInt(15) + 1;
+    		}
+    		else {
+    			coins[i] = 1; // assure the sum can be made
+    		}
+    	}
+		
+		// test that the sum is correct
+		int sum = 0;
+		for (int i : testCoins.makeChange(coins, change) ) {
+			sum += i;
+		}
+		assertEquals(change, sum);
+    }
+    
+    @Test
+    void testImpossibleInput() {
+    	int[] coins = {5, 17};
+    	int change = 13;
+		
+		// correct change is impossible since there are no "1"s
+		int sum = 0;
+		for (int i : testCoins.makeChange(coins, change) ) {
+			sum += i;
+		}
+		assertNotEquals(change, sum);
+		
+		Exception e = assertThrows(IllegalArgumentException.class, () -> {  testCoins.makeChange(coins, change); } );
+		// test correct error message is generated
+		assertEquals("Must be given a set of coins that can sum to the change value", e.getMessage());
+    }
 	
 	@Test
 	void testAll5s() {
 		int[] coins = {1, 5, 17};
 		int[] expected = {5, 5, 5};
+		int change = 15;
 		
 		// test correct change is made
 		int sum = 0;
-		for (int i : testCoins.makeChange(coins, 15) ) {
+		for (int i : testCoins.makeChange(coins, change) ) {
 			sum += i;
 		}
-		assertEquals(15, sum);
+		assertEquals(change, sum);
 			
 		// test correct array returns
 		assertArrayEquals(expected,   testCoins.makeChange(coins, 15));
@@ -45,13 +95,14 @@ class TestCoins {
 	void testMixedReturn() {
 		int[] coins = {1, 5, 10, 25};
 		int[] expected = {25, 10, 1, 1};
+		int change = 37;
 		
 		// test correct change is made
 		int sum = 0;
-		for (int i : testCoins.makeChange(coins, 37) ) {
+		for (int i : testCoins.makeChange(coins, change) ) {
 			sum += i;
 		}
-		assertEquals(37, sum);
+		assertEquals(change, sum);
 		
 		// test correct coins return
 		assertArrayEquals(expected,   testCoins.makeChange(coins, 37));
@@ -62,13 +113,14 @@ class TestCoins {
 		int[] coins = {1, 5, 11};
 		// this is the correct answer but a greedy algorithm will give {11, 5, 1, 1, 1, 1}
 		int[] expected = {5, 5, 5, 5};
+		int change = 20;
 		
 		// test correct change is made
 		int sum = 0;
-		for (int i : testCoins.makeChange(coins, 20) ) {
+		for (int i : testCoins.makeChange(coins, change) ) {
 			sum += i;
 		}
-		assertEquals(20, sum);
+		assertEquals(change, sum);
 		
 		// test correct coins return
 		assertEquals(expected,   testCoins.makeChange(coins, 20));
